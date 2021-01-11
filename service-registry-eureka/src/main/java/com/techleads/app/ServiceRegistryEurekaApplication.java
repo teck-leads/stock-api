6738089b7907128e87575d37efd629b903daf80d
TechLeads <anupoju.anand@gmail.com>
2021-01-11 20:16:42 +0530

    Service-Registry-Eureka Server

---
+++ service-registry-eureka/src/main/java/com/techleads/app/ServiceRegistryEurekaApplication.java
@@ -0,0 +1,15 @@
+package com.techleads.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+@EnableEurekaServer
+@SpringBootApplication
+public class ServiceRegistryEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistryEurekaApplication.class, args);
+	}
+
+}
